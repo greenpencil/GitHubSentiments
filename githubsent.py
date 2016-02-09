112dae08a4c90665a155c1dce18b3a4ce41278fe
Hussein Mohammed <zidhussein@gmail.com>
2016-02-09 17:00:34 +0000

    Made changes

--- githubsent.py
+++ githubsent.py
@@ -1,12 +1,18 @@
-import nltk
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+import sys
 from github import Github
-import vaderSentiment
 from nltk.sentiment import SentimentIntensityAnalyzer
 
-g = Github("greenpencil", "")
+g = Github("github_username", "github_password")
 
-username = input("username: ")
-repo = input("repo: ")
+if len(sys.argv) > 1:
+    username = sys.argv[1]
+    repo = sys.argv[2]
+else:
+    username = input("username: ")
+    repo = input("repo: ")
 
 sid = SentimentIntensityAnalyzer()
 
@@ -14,5 +20,5 @@ for commit in g.get_user(username).get_repo(repo).get_commits():
     print(commit.commit.message)
     ss = sid.polarity_scores(commit.commit.message)
     for k in sorted(ss):
-         print('{0}: {1}, '.format(k, ss[k]), end='')
-         print()
+        print('{0}: {1}, '.format(k, ss[k]), end='')
+        print()
